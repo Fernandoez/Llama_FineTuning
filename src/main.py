5d630436e344ec3754e55b04b197dd11eedb16cd
Fernandoez <fernando.ezimerman@gmail.com>
2026-02-11 18:12:25 -0300

    Mudando caminho dos outputs

--- src/main.py
+++ src/main.py
@@ -6,14 +6,14 @@ from data.inferences_data import THEMES, TOPICS_RESTRICTIONS, QUESTION_FORMAT
 from src.utils import save_results
 from pathlib import Path
 
-OUT_DIR = Path("/app/output")
+OUT_DIR = Path("/output")
 OUT_DIR.mkdir(exist_ok=True)
 
 def main():
     model, tokenizer = load_model()
     print("--- Início das inferências antes do FT ---")
     results = run_inference(model, tokenizer, THEMES, TOPICS_RESTRICTIONS, QUESTION_FORMAT)
-    save_results(results, outputh_path_xlsx = "/app/output/questoes_geradas_ZS.xlsx", outputh_path_json= "/app/output/questoes_geradas_ZS.json")
+    save_results(results, outputh_path_xlsx = "output/questoes_geradas_ZS.xlsx", outputh_path_json= "output/questoes_geradas_ZS.json")
     print("--- Fim das inferências antes do FT ---")
     print("--- Início do fine-tuning ---")
     dataset = load_and_prepare_dataset("data/questoes.json", tokenizer)
@@ -21,7 +21,7 @@ def main():
     print("--- Fim do fine-tuning ---")
     print("--- Início das inferências depois do FT ---")
     results = run_inference(model, tokenizer, THEMES, TOPICS_RESTRICTIONS, QUESTION_FORMAT)
-    save_results(results, outputh_path_xlsx = "/app/output/questoes_geradas_FT.xlsx", outputh_path_json= "/app/output/questoes_geradas_FT.json")
+    save_results(results, outputh_path_xlsx = "output/questoes_geradas_FT.xlsx", outputh_path_json= "output/questoes_geradas_FT.json")
     print("--- Fim das inferências depois do FT ---")
 
 if __name__ == "__main__":
